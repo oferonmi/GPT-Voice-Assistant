54e7bd4be7c57d1ecd0b04987177070baf10b3f8
Victor Ocheri <datafilik@gmail.com>
2023-03-07 22:18:46 +0000

    added function to utilize gpt-3.5-turbo model

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ engine = tts.init()
 # voices = engine.getProperty('voices')
 # engine.setProperty('voice', voices[-1].id)
 
-
 def sr_transcribe_audio_to_text(file_name):
   recognizer = sr.Recognizer()
   with sr.AudioFile(file_name) as source:
@@ -38,7 +37,7 @@ def whisper_transcribe_audio_to_text(file_name):
     print("Ah, let's give this another try.")
 
 
-def generate_response(prompt):
+def generate_prompt_response(prompt):
   response = openai.Completion.create(engine="text-davinci-003",
                                       prompt=prompt,
                                       max_tokens=4000,
@@ -48,34 +47,53 @@ def generate_response(prompt):
   return response["choices"][0]["text"]
 
 
+def generate_chat_response(msg):
+  response = openai.ChatCompletion.create(
+      model="gpt-3.5-turbo",
+      messages=[
+          {"role": "user", "content": msg}
+      ]
+  )
+  return response.choices[0].message["content"]
+  # return response["choices"][0]["message"]["content"]
+
+
 def speak(text):
   engine.say(text)
   engine.runAndWait()
 
 
-def main(): 
+# main
+def main(gpt_version): 
+  # Input - gpt_version takes valid values of 'gpt-3' and 'gpt-3.5'
   while True:
     # TODO find and fix cause of the ALSA dumps
     with noalsaerr():  # suppress ALSA lib dumps for now. remove when dump issues are fixed
       recognizer = sr.Recognizer()
       # initial prompt ('Hey! Kai' in this case) to begin recording
       print("Say 'Hey Kai' to record your question...")
-      #time.sleep(2)
+      time.sleep(1)
       with sr.Microphone() as source:
         recognizer.adjust_for_ambient_noise(source)
         audio = recognizer.listen(source)
         try:
           transcription = recognizer.recognize_google(audio)
           #if transcription.lower == "lex":
+
+          print("I am listening. Ask your question ...")
+          speak("Yes. Ask your question")
+
           # record audio
           in_audio_filename = "input.wav"
-          print("I am listening. Ask your question ...")
+
           with sr.Microphone() as source:
             source.pause_threshold = 1
             recognizer.adjust_for_ambient_noise(source)
+            
             audio = recognizer.listen(source,
                                       phrase_time_limit=None,
-                                      timeout=None)
+                                      timeout=None)  
+
             with open(in_audio_filename, "wb") as f:
               f.write(audio.get_wav_data())
 
@@ -86,8 +104,12 @@ def main():
             print("You said: '{}'.".format(text))
 
             # generate response
-            response = generate_response(text)
-            print("GPT-3 response: {}".format(response))
+            if gpt_version == "gpt-3":
+              response = generate_prompt_response(text)
+            elif gpt_version == "gpt-3.5":
+              response = generate_chat_response(text)
+
+            print("GPT response: {}".format(response))
 
             #read out response
             speak(response)
@@ -97,10 +119,12 @@ def main():
 
         except sr.UnknownValueError:
           print("Unable to recognize speech")
+          speak("I could not hear you.")
 
         # except sr.RequestError:  # Exception as e:
         #   print("An error occured: {}".format(e))
     # TODO create way to end question and answer loop smoothly
 
+
 if __name__ == "__main__":
-  main()
+  main("gpt-3") # change parameter to gpt-3.5 to use the latest gpt-3.5-turbo model
