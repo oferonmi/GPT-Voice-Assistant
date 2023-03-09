3351231777f79ba9501c432b4e34eea9be93190e
Victor Ocheri <datafilik@gmail.com>
2023-03-09 01:01:31 +0000

    fixed issue with idle sound at voice prompt

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 import openai
 import pyttsx3 as tts
+from gtts import gTTS
+# from playsound import playsound
 import speech_recognition as sr
 from pygame import mixer
 import time
-
 from py_error_handler import noalsaerr # to suppress ALSA lib dumps
-
 import os
-
 from dotenv import load_dotenv
-load_dotenv() # load environment variable from .env file
+
+activate_idle_state_sound = True # Set to deactivate sound that play in idle sessions
+
+# load environment variable from .env file
+load_dotenv() 
 
 # get OpenAI API key
 openai.api_key =  os.environ['OPENAI_API_KEY'] # os.getenv("OPENAI_API_KEY")
 
+# initialize module for loading and playing sounds
+mixer.init()
+
 # text-to-speech engine instance
 tts_engine = tts.init()
+
+# TODO add a much smoother speech synthesizer 
 # voices = tts_engine.getProperty('voices')
+# print("Number of voices: {}".format(len(voices)))
 # tts_engine.setProperty('voice', voices[-1].id)
 
 # functions for transcribing sound from file to text
@@ -59,18 +68,27 @@ def generate_chat_response(msg):
   return response.choices[0].message["content"]
   # return response["choices"][0]["message"]["content"]
 
-# transcribe text string to sound
+# functions to transcribe text string to sound
 def speak(text):
   tts_engine.say(text)
   tts_engine.runAndWait()
 
+# TODO issue with text incomplete text read out
+def tts_speak(text):
+  tts = gTTS(text, lang="en", tld="co.uk")
+  tts.save("out.mp3")
+  # playsound("out.mp3")
+  play_sound_file("out.mp3")
+
 
 def play_sound_file(sound_file):
   # input - sound_file specifies path to sound file in string
-  mixer.init()
+  # mixer.init()
   mixer.music.load(sound_file)
   mixer.music.play()
-  # mixer.music.stop()
+
+def stop_playing_sound_file_gracefully():
+  mixer.music.fadeout(2)
 
 # main
 def main(gpt_version): 
@@ -82,8 +100,9 @@ def main(gpt_version):
     # TODO find and fix cause of the ALSA dumps
     with noalsaerr():  # suppress ALSA lib dumps for now. remove when dump issues are fixed
       recognizer = sr.Recognizer()
-      # initial prompt ('Hey! Kai' in this case) to begin recording
-      print("Say 'Hey Kai' to record your question...")
+
+      # initial prompt ('Kai' in this case) to begin recording
+      print("Say 'Kai' to record your question...")
       
       with sr.Microphone() as source:
         source.pause_threshold = 1
@@ -92,8 +111,13 @@ def main(gpt_version):
 
         try:
           transcription = recognizer.recognize_google(audio)
-          #if transcription.lower == "lex":
 
+          # stop sound  triggered by possible previous inactivity
+          if no_activity_period_count > 0:
+            if activate_idle_state_sound:
+              stop_playing_sound_file_gracefully()
+
+          #if transcription.lower == "kai":
           print("Yes. How may I be of help?")
           speak("Yes. How may I be of help?")
 
@@ -129,34 +153,32 @@ def main(gpt_version):
             #read out response
             speak(response)
             speak("I hope that helped. You can ask your next question.")
+            time.sleep(2)
+
             # reset counter
             no_activity_period_count = 0 
 
         except sr.RequestError: 
           print("API unavailable")
+          speak("It would appear we have a problem with access to connectivity.")
 
         except sr.UnknownValueError:
           print("Unable to recognize speech")
           # speak("I could not hear you.") 
           # play a brief sound file for non active period
-          play_sound_file("ambient-music-0036.mp3")
+          if activate_idle_state_sound:
+            play_sound_file("ambient-music-0036.mp3")
           
-          #print(no_activity_period_count)
+          # keep track of non activity
           no_activity_period_count += 1
 
     # Temporal way to end question and answer loop due to non activity
-    # stops session after 3 instance of no prompts
-    if no_activity_period_count > 3:
+    # stops session after 3 instance of no prompts.
+    if no_activity_period_count > 5:
       time.sleep(2)
       speak("Bye for now.")
       break
-
-
-
-        # except sr.RequestError:  # Exception as e:
-        #   print("An error occured: {}".format(e))
-    # TODO create way to end question and answer loop smoothly
-
-
+  
+# main 
 if __name__ == "__main__":
   main("gpt-3.5") # change parameter to gpt-3.5 to use the latest gpt-3.5-turbo model
